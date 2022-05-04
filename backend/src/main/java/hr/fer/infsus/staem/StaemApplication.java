6108b27b83616f144754203dbadb42a24f547313
Nikola Kescec <nikola.kescec@fer.hr>
2022-05-04 20:02:03 +0200

    backend init

---
+++ backend/src/main/java/hr/fer/infsus/staem/StaemApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.infsus.staem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaemApplication.class, args);
+	}
+
+}
